c44613ab91af5d507004a0051186a0ff2925a959
BC <boeunwork@gmail.com>
2025-08-10 17:56:00 -0400

    Built API endpoints and Swagger UI

---
+++ helpdesk_api/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import TicketListCreateAPIView
+
+urlpatterns = [
+    path('tickets/', TicketListCreateAPIView.as_view(), name='ticket-list-create'),
+]
--- helpdesk_api/views.py
+++ helpdesk_api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Ticket
+from .serializers import TicketSerializer
 
 # Create your views here.
+class TicketListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
